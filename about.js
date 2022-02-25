1253d1165bd1c525128123839b77f66a9dae5b3d
Jacob Morse <jacobsmorse@gmail.com>
2022-02-25 11:50:58 -0700

    step 5 completed

--- about.js
+++ about.js
@@ -4,11 +4,15 @@ console.log("hello world");
 
 function handleSubmit(evt) {
 	evt.preventDefault();
-	
-	console.log('form submit');
+	alert("Form was submitted successfully!")
+	// console.log('form submit');
 }
 
 
 let form = document.querySelector('form#contact');
 
 form.addEventListener('submit', handleSubmit);
+
+document.getElementById('duck-pic').addEventListener('mouseover', () => {
+	alert("You are so charming!")
+})
---
+++ profile.js
@@ -0,0 +1,11 @@
+document.getElementById('color').addEventListener('click', () => {
+    alert("My favorite color is Purple!")
+})
+
+document.getElementById('place').addEventListener('click', () => {
+    alert("My favorite place is the mountains.")
+})
+
+document.getElementById('ritual').addEventListener('click', () => {
+    alert("My favorite ritual is walking my dog.")
+})
